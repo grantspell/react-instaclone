1408433e10491a812a54f584386bbf452735c013
grantspell <spellgrant@gmail.com>
2017-10-11 14:05:40 -0400

    | INITIAL COMMIT |

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import Posts from './components/Posts';
 import Navbar from './components/Navbar';
+import styled from 'styled-components';
+
+const Body =
+  styled.div`
+    background: rgb(249, 249, 249);
+    font-family: "Open Sans";
+  `;
 
 class App extends Component {
   render() {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,38 @@ import React, { Component } from 'react';
 import { FaInstagram, FaUser, FaHeartO, FaCompass, FaSearch } from 'react-icons/lib/fa';
 import styled from 'styled-components';
 
+
+const FlexRow = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const NavbarWrapper = FlexRow.div`
+
+`;
+
+/* const Wrapper = ({ className, children }) => (
+  <div className={className}>{children}</div>
+)
+
+const StyledWrapper = styled(Wrapper)`
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+  -webkit-box-align: center;
+  align-items: center;
+  padding: 26px 5vw;
+  width: 90vw;
+  height: 25px;
+  border-bottom: 1px solid rgba(0, 0, 0, .0975);
+`; */
+
 class Navbar extends Component{
+
   render(){
     return (
       <div>
+      <StyledWrapper>
         <h3><FaInstagram /> | Instaclone</h3>
         <FaSearch/><input type="text"/>
         <div>
@@ -13,6 +41,7 @@ class Navbar extends Component{
           <FaHeartO />
           <FaUser />
         </div>
+      </StyledWrapper>
       </div>
     );
   }
